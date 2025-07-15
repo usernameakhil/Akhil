3d7b9ff67f2b636a9da87ff4c0c44165c3b7c00b
Akhil <shaikmohammadakhil290406@gmail.com>
2025-07-15 15:49:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const maskLayer = document.querySelector('.profile-pic-border');
+const container = document.querySelector('.section__pic-container');
+
+container.addEventListener('mousemove', (e) => {
+  const rect = container.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  const maskStyle = `radial-gradient(circle 80px at ${x}px ${y}px, transparent 100%, black 100%)`;
+  maskLayer.style.maskImage = maskStyle;
+  maskLayer.style.webkitMaskImage = maskStyle;
+});
+
+container.addEventListener('mouseleave', () => {
+  const maskStyle = `radial-gradient(circle 80px at -200px -200px, transparent 100%, black 100%)`;
+  maskLayer.style.maskImage = maskStyle;
+  maskLayer.style.webkitMaskImage = maskStyle;
+});
